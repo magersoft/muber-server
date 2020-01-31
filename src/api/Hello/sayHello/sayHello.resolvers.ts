6fd087e62357f58ebaec34033a22548ee8818ba1
v.mager <v.mager@b2b-center.ru>
2020-01-31 11:56:44 +0300

    DB connection to postgreSql

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,6 +1,13 @@
+import { SayHelloQueryArgs, SayHelloResponse } from '../../../types/graph';
+
 const resolvers = {
   Query: {
-    sayHello: () => 'Hey hello from graphql'
+    sayHello: (_, args: SayHelloQueryArgs): SayHelloResponse => {
+      return {
+        error: false,
+        text: `Hello! ${args.name}`
+      }
+    }
   }
 };
 
